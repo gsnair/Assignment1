5ba6f958862b3bd20cf2fa48ea365040d30125f7
gokul nair <gsnair@gmail.com>
2017-05-06 12:33:22 -0700

    Original commit for Assignment 1

---
+++ Assignment1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello Foothill College Students!\n\n";
+   cout << "My family name is Nair.\n";
+   cout << "My student ID is 20118453.\n\n";
+   cout << "Syllabus Detail #1: The listed text for this course is recommended"
+   "\nbut not required. Having at least some reference is required.\n\n.";
+   cout << "Syllabus Detail #2: Style guidelines in this course serve as prac-"
+   "\n-tice for customer-focused programming.\n\n";
+   cout << "Specific Lab Detail #1: Late assignments must be turned in no lat-"
+   "\n-er than 1 week after the posted deadline.\n\n";
+   cout << "Specific Lab Detail #2: I/O should never, ever be mixed with calc-"
+   "\n-ulations.\n\n";
+   cout << "Specific Lab Detail #3: In general, single-letter variable names"
+   "\nshould be avoided. Exceptions are made for counters/indices.\n\n";
+   return 0;
+}
+
+/*-----------------OUTPUT-----------------
+Hello Foothill College Students!
+
+My family name is Nair.
+My student ID is 20118453.
+
+Syllabus Detail #1: The listed text for this course is recommended
+but not required. Having at least some reference is required.
+
+.Syllabus Detail #2: Style guidelines in this course serve as prac-
+-tice for customer-focused programming.
+
+Specific Lab Detail #1: Late assignments must be turned in no lat-
+-er than 1 week after the posted deadline.
+
+Specific Lab Detail #2: I/O should never, ever be mixed with calc-
+-ulations.
+
+Specific Lab Detail #3: In general, single-letter variable names
+should be avoided. Exceptions are made for counters/indices.
+
+Press any key to continue . . .
+------------------------------------------*/
+
